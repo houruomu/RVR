6a6c56d8bdf9c1ef668bc6982024bd739107b20d
Ruomu <houruomu@gmail.com>
2018-09-11 16:42:32 +0800

    contoller autotest module added

--- algorithm/Controller.go
+++ algorithm/Controller.go
@@ -35,6 +35,42 @@ type ControllerState struct {
 	Address     string
 	ServerList  []string
 }
+func (c *ControllerState) checkConnection(){
+	connectedPeers := make([]message.Identity, 0)
+	for i, _ := range c.PeerList{
+		client, err := rpc.Dial("tcp", c.PeerList[i].Address)
+		if err != nil {
+			log.Print("Peer %s disconnected.\n", c.PeerList[i].Address)
+			continue
+		}
+		connectedPeers = append(connectedPeers, c.PeerList[i])
+		client.Close()
+	}
+	c.PeerList = connectedPeers
+
+	connectedServers := make([]string, 0)
+	for i, _ := range c.ServerList{
+		client, err := rpc.Dial("tcp", c.ServerList[i])
+		if err != nil {
+			log.Print("Peer %s disconnected.\n", c.ServerList[i])
+			continue
+		}
+		connectedServers = append(connectedServers, c.ServerList[i])
+		client.Close()
+	}
+	c.ServerList = connectedServers
+}
+
+func (c *ControllerState) spawnEvenly(count int){
+	c.checkConnection()
+	for i, _ := range c.ServerList{
+		numInstance := count / len(c.ServerList)
+		if numInstance < count % len(c.ServerList){
+			numInstance++
+		}
+		c.Spawn(c.ServerList[i], numInstance)
+	}
+}
 
 func (c *ControllerState) Spawn(addr string, count int) {
 	c.ServerList = append(c.ServerList, addr)
@@ -58,7 +94,7 @@ func (c *ControllerState) Register(id message.Identity, rtv *int) error {
 	return nil
 }
 
-func (c *ControllerState) SetupRandomizedView(ph1 int, ph2 *int) error {
+func (c *ControllerState) setupRandomizedView() error {
 	for i, _ := range c.PeerList {
 		view := make([]uint64, 0)
 		for j, _ := range c.PeerList {
@@ -90,7 +126,7 @@ func (c *ControllerState) SetupProtocol(ph1 int, ph2 *int) error {
 		defer client.Close()
 		client.Go("ProtocolState.Setup", c.SetupParams, nil, nil)
 	}
-	c.SetupRandomizedView(1, nil)
+	c.setupRandomizedView()
 	print(c.SetupParams.String())
 	return nil
 }
@@ -191,6 +227,8 @@ func (c *ControllerState) StartListen() {
 		for scanner.Scan() {
 			text := scanner.Text()
 			switch text {
+			case "auto":
+				c.autoTest(10, DefaultSetupParams)
 			case "setup":
 				c.SetupProtocol(1, nil)
 			case "start":
@@ -219,6 +257,36 @@ func (c *ControllerState) StartListen() {
 	}
 
 }
+
+func (c *ControllerState) autoTest(size int, params ProtocolRPCSetupParams){
+	c.KillNodes(1, nil)
+	c.PeerList = make([]message.Identity, 0)
+
+	for len(c.PeerList) < size{
+		c.spawnEvenly(size - len(c.PeerList))
+		time.Sleep(1000 * time.Millisecond)
+	}
+
+	c.SetupParams = params
+	c.SetupProtocol(1, nil)
+	c.StartProtocol(1, nil)
+
+	print("Auto-test starting!\n")
+	var report string
+	for{
+		time.Sleep(10 * time.Second)
+		report = c.report()
+		if report[0] == 't'{
+			// finished
+			break
+		}
+		print("protocol haven't finished!\n")
+	}
+	print("Auto-test completed!\n")
+	fmt.Printf("%d, %d, %s", size, len(c.PeerList), report)
+}
+
+
 
 func StartServer(exitSignal chan bool){
 	c := ControllerState{exitSignal, DefaultSetupParams, make([]message.Identity, 0), "", make([]string, 0)}
--- compile.bat
+++ compile.bat
@@ -1,5 +1,7 @@
 env GOOS=linux GOARCH=amd64 go build
 env GOOS=windows GOARCH=amd64 go build
 git add *
-git commit -m "add report function to controller"
+git commit -m "contoller autotest module added"
 git push
+
+exit
