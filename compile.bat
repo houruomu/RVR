8826a013603af4e6e2f139c8f34e6fe07e351e49
Ruomu <houruomu@gmail.com>
2018-09-12 17:11:55 +0800

    update version number

--- compile.bat
+++ compile.bat
@@ -1,7 +1,7 @@
 env GOOS=linux GOARCH=amd64 go build
 env GOOS=windows GOARCH=amd64 go build
 git add *
-git commit -m "make auto testing more robust under failure"
+git commit -m "update version number"
 git push
 
 exit
